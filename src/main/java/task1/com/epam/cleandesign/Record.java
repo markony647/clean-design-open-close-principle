ab71dbce8b444aefc443cf62ff35f0305d576b61
Andrii Shylin <andrii.shylin@epam.com>
2021-09-22 15:48:54 +0300

    Initial commit

---
+++ src/main/java/task1/com/epam/cleandesign/Record.java
@@ -0,0 +1,14 @@
+package task1.com.epam.cleandesign;
+
+public class Record {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
