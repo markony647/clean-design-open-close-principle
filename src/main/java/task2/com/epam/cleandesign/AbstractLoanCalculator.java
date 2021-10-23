32048b84f86c2f4aac7fde687058ae43c7e875fa
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-10-23 20:04:06 +0300

    Add applying multiplier to the loan in abstract class

--- src/main/java/task2/com/epam/cleandesign/AbstractLoanCalculator.java
+++ src/main/java/task2/com/epam/cleandesign/AbstractLoanCalculator.java
@@ -1,5 +1,17 @@
 package task2.com.epam.cleandesign;
 
 public abstract class AbstractLoanCalculator {
-    abstract int calculate(int age, int income);
+
+    private final IncomeMultiplier incomeMultiplier;
+
+    public AbstractLoanCalculator(IncomeMultiplier incomeMultiplier) {
+        this.incomeMultiplier = incomeMultiplier;
+    }
+
+    abstract int calculateBeforeApplyingMultiplier(int age, int income);
+
+    public int calculate(int age, int income) {
+        int loan = calculateBeforeApplyingMultiplier(age, income);
+        return incomeMultiplier.multiply(loan, income);
+    }
 }
